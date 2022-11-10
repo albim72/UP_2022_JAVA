e686f9b61f2fe9d6e7eb22f07f0fc8a88a88d2ef
albim72 <marcin.albiniak72@gmail.com>
2022-11-10 19:07:39 +0100

    Update TrzeciaKlasa.java

--- DZIEN_1/klasa123/TrzeciaKlasa.java
+++ DZIEN_1/klasa123/TrzeciaKlasa.java
@@ -1,18 +1,18 @@
 public class TrzeciaKlasa extends DrugaKlasa{
 
     public int d;
-    public TrzeciaKlasa(int a, int b, int c, int d) {
-        super(a, b, c);
+    public TrzeciaKlasa(int a, int b,int g, int h, int c, int d) {
+        super(a, b, g, h, c);
         this.d = d;
     }
 
     public void print_abcd(){
 
-        System.out.printf("Wartości: a = %d, b = %d, c = %d, d = %d",a,b,c,d);
+        System.out.printf("Wartości: a = %d, b = %d, c = %d, d = %d\n",a,b,c,d);
     }
 
     @Override
     public int sumuj() {
-        return a+b+c+d;
+        return a+b+c+d+oblicz();
     }
 }
