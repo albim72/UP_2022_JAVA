22b4de2f3941391a7c9f36cdca06fd6e3dcf639a
albim72 <marcin.albiniak72@gmail.com>
2022-11-10 19:07:14 +0100

    Update DrugaKlasa.java

--- DZIEN_1/klasa123/DrugaKlasa.java
+++ DZIEN_1/klasa123/DrugaKlasa.java
@@ -1,16 +1,16 @@
 public class DrugaKlasa extends PierwszaKlasa {
 
     public int c;
-    public DrugaKlasa(int a, int b, int c) {
-        super(a, b);
+    public DrugaKlasa(int a, int b,int g, int h, int c) {
+        super(a, b, g, h);
         this.c = c;
     }
 
     public void print_abc(){
 
-        System.out.printf("Wartości: a = %d, b = %d, c = %d",a,b,c);
+        System.out.printf("Wartości: a = %d, b = %d, c = %d\n",a,b,c);
     }
-    
+
     public int sumuj(){
         return a+b+c;
     }
