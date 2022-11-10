2f683554fafdecd480d312dd190828c87f826374
albim72 <marcin.albiniak72@gmail.com>
2022-11-10 19:53:53 +0100

    Create Main.java

---
+++ DZIEN_1/figury/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Prostokat pr = new Prostokat(5.5,6.1);
+        System.out.printf("Pole prostokąta: %.2f\n",pr.PoleFigury());
+
+        Trojkat tr = new Trojkat(4.6,6.8);
+        System.out.printf("Pole trójkąta: %.2f\n",tr.PoleFigury());
+
+        Trapez trp = new Trapez(6.8,4.9,4.4);
+        System.out.printf("Pole trapezu: %.2f\n",trp.PoleFigury());
+    }
+}
