a62b33e15027fa77eda93c800c0308304233db42
albim72 <marcin.albiniak72@gmail.com>
2022-11-10 19:21:57 +0100

    Create Main.java

---
+++ DZIEN_1/abstrakcja/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Zwykla z = new Zwykla(5.5,7,8.2);
+        System.out.printf("wynik policz() -> %.2f\n",z.policz());
+        System.out.printf("wynik policz_a() -> %.2f\n",z.policz_a());
+    }
+}
