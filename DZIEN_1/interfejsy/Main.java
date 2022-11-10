8b2ff91b5d8d5e179b5347df8a45aeb5198864e5
albim72 <marcin.albiniak72@gmail.com>
2022-11-10 20:21:37 +0100

    Create Main.java

---
+++ DZIEN_1/interfejsy/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Zbiorcza z = new Zbiorcza();
+        System.out.println(z.wynikA(2.4,56.7));
+        System.out.println(z.liczba(3,7));
+        System.out.println(z.messg("takie tam 787"));
+        System.out.println(z.wynik2A());
+    }
+}
