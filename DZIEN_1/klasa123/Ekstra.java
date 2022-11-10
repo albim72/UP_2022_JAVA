f82c944fdc36892c9d26d0749770613d309a080a
albim72 <marcin.albiniak72@gmail.com>
2022-11-10 18:47:21 +0100

    Create Ekstra.java

---
+++ DZIEN_1/klasa123/Ekstra.java
@@ -0,0 +1,14 @@
+public class Ekstra {
+
+    int g;
+    int h;
+
+    public Ekstra(int g, int h) {
+        this.g = g+2;
+        this.h = h*3;
+    }
+
+    public int oblicz(){
+        return g*h;
+    }
+}
