ce25fd24af512ee0de28ebf60bd01c0e15929c21
albim72 <marcin.albiniak72@gmail.com>
2022-11-10 19:06:52 +0100

    Update PierwszaKlasa.java

--- DZIEN_1/klasa123/PierwszaKlasa.java
+++ DZIEN_1/klasa123/PierwszaKlasa.java
@@ -1,8 +1,9 @@
-public class PierwszaKlasa {
+public class PierwszaKlasa extends Ekstra {
     int a;
     int b;
 
-    public PierwszaKlasa(int a, int b) {
+    public PierwszaKlasa(int a, int b,int g,int h) {
+        super(g,h);
         this.a = a;
         this.b = b;
     }
@@ -10,6 +11,6 @@ public class PierwszaKlasa {
     public void print_ab(){
         //System.out.println("Wartości a="+a+", b="+b);
         //skrót souf
-        System.out.printf("Wartości: a = %d, b = %d",a,b);
+        System.out.printf("Wartości: a = %d, b = %d\n",a,b);
     }
 }
