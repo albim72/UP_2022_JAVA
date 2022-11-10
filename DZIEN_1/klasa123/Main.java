3e9b0ea1cc6456d49c359da3e86b34cff7ee4eb5
albim72 <marcin.albiniak72@gmail.com>
2022-11-10 19:08:26 +0100

    Update Main.java

--- DZIEN_1/klasa123/Main.java
+++ DZIEN_1/klasa123/Main.java
@@ -2,21 +2,23 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("__________________PierwszaKlasa______________________");
-        PierwszaKlasa pk = new PierwszaKlasa(4,7);
+        PierwszaKlasa pk = new PierwszaKlasa(4,7,2,2);
         pk.print_ab();
 
         System.out.println("___________________DrugaKlasa______________________");
 
-        DrugaKlasa dk = new DrugaKlasa(5,2,8);
+        DrugaKlasa dk = new DrugaKlasa(5,2,8,2,4);
         dk.print_ab();
         dk.print_abc();
         System.out.println(dk.sumuj());
         System.out.println("__________________TrzeciaKlasa______________________");
 
-        TrzeciaKlasa tk = new TrzeciaKlasa(6,3,8,11);
+        TrzeciaKlasa tk = new TrzeciaKlasa(6,3,8,11,4,5);
         tk.print_ab();
         tk.print_abc();
         tk.print_abcd();
         System.out.println(tk.sumuj());
+        System.out.println(tk.g);
+        System.out.println(tk.h);
     }
 }
