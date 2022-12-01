6d2f33d7d61006f7d5e8a1bde8375799eb1af5b5
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 18:33:18 +0100

    Create EOpisBmi.java

---
+++ DZIEN_2/FITNESS/EOpisBmi.java
@@ -0,0 +1,14 @@
+public enum EOpisBmi {
+    NIEDOWAGA("masz niedowagę"),
+    PRAWIDLOWA("twoja wagajest prawidłowa!"),
+    NADWAGA("masz nadowagę"),
+    OTYLOSC1("otyłość I stopnia"),
+    OTYLOSC2("otyłość II stopnia"),
+    OTYLOSC3("otyłość III stopnia");
+
+    private final String info;
+
+    EOpisBmi(String info) {
+        this.info = info;
+    }
+}
