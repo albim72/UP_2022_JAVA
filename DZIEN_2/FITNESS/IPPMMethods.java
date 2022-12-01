8f1941bd33ce50b48b4aafdf22644b06e56abd7f
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 18:39:48 +0100

    Create IPPMMethods.java

---
+++ DZIEN_2/FITNESS/IPPMMethods.java
@@ -0,0 +1,6 @@
+public interface IPPMMethods {
+    public double ppm1kob(double waga,double wzrost, int wiek);
+    public double ppm2kob(double waga,double wzrost, int wiek);
+    public double ppm1men(double waga,double wzrost, int wiek);
+    public double ppm2men(double waga,double wzrost, int wiek);
+}
