f26030f5e099f0162aaaf4108cb10cef5b9a8801
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 20:14:45 +0100

    Create Area.java

---
+++ DZIEN_2/area_project/Area.java
@@ -0,0 +1,14 @@
+public class Area {
+    int length;
+    int breadth;
+
+    public Area(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+    
+    public void getArea(){
+        int area = length*breadth;
+        System.out.println("Pole: "+area);
+    }
+}
