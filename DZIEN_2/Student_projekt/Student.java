d09ee9215a4ffaba5d2da722ccc12ecad7e619dc
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 20:09:36 +0100

    Create Student.java

---
+++ DZIEN_2/Student_projekt/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public void StudentId(String name, int nralb){
+        System.out.println("Imię: "+name+", nr albumu:" + nralb);
+    }
+
+    public void StudentId(int nralb,String name){
+        System.out.println("nr albumu:" + nralb + ", Imię: "+name);
+    }
+}
