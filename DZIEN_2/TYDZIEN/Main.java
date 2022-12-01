35e91c7b993eb28d3011fcd0ccf580757dfcb205
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 18:18:15 +0100

    Create Main.java

---
+++ DZIEN_2/TYDZIEN/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "SUNDAY";
+        Tydzien t1 = new Tydzien(Dzien.valueOf(str));
+        t1.dzienJak();
+    }
+}
