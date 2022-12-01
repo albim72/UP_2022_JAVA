8176ff466870e7c9f1f62ea9eb421c6557854c29
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 18:36:25 +0100

    Create FWriter.java

---
+++ DZIEN_2/FITNESS/FWriter.java
@@ -0,0 +1,11 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FWriter {
+    public FWriter() {
+    }
+    FileWriter fw = null;
+    public void mfWrite() throws IOException {
+        fw = new FileWriter("c:\\Temp\\daneFitness.txt");
+    }
+}
