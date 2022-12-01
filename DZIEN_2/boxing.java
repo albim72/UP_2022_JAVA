2e511cc07e83c0fa8b3d82d24fd8422e14c1a261
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 19:49:57 +0100

    Create boxing.java

---
+++ DZIEN_2/boxing.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+      char ch='a';
+      Character a = ch;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(25);
+        int num = arrayList.get(0);
+        System.out.println(num);
+
+    }
+}
