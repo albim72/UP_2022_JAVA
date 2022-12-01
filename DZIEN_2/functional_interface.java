c97a7c388a666b4667531292c41e2b6550f09f8a
albim72 <marcin.albiniak72@gmail.com>
2022-12-01 20:38:22 +0100

    Create functional_interface.java

---
+++ DZIEN_2/functional_interface.java
@@ -0,0 +1,14 @@
+@FunctionalInterface
+interface Square{
+    int calculate(int x);
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 5;
+        Square s = (int x) -> x*x;
+        int ans = s.calculate(a);
+        System.out.println(ans);
+    }
+}
