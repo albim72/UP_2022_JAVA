7bbfad9bf01fc772758c8f12b5c29c1fdefe537e
albim72 <marcin.albiniak72@gmail.com>
2022-12-15 17:40:07 +0100

    Update Main.java

--- DZIEN_4/MOj_EXCEPTION/Main.java
+++ DZIEN_4/MOj_EXCEPTION/Main.java
@@ -2,10 +2,17 @@ public class Main {
     public static void main(String[] args) {
 
         try{
-            throw new MojWyjatek("To jest błąd opisany przez Marcina....");
-        }catch (MojWyjatek ex){
+
+            double a=7.8, b = 7.8, c = 2.2;
+            double wynik = (c+b)/(a-b);
+            throw new MojeZero("a powinno być różne od b!");
+            //throw new MojWyjatek("To jest błąd opisany przez Marcina....");
+
+       /* }catch (MojWyjatek ex){
             System.out.println("przechwycony wyjątek");
-            System.out.println(ex.getMessage());
+            System.out.println(ex.getMessage());*/
+        }catch (MojeZero z){
+            System.out.println(z.getMessage());
         }
     }
 }
