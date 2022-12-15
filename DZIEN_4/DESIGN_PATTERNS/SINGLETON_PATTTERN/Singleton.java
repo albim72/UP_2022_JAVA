e907d0e5e006f42fcf97471e0d52295287e3c167
albim72 <marcin.albiniak72@gmail.com>
2022-12-15 18:15:46 +0100

    Create Singleton.java

---
+++ DZIEN_4/DESIGN_PATTERNS/SINGLETON_PATTTERN/Singleton.java
@@ -0,0 +1,21 @@
+public final class Singleton {
+    private static volatile Singleton instance;
+    public String value;
+
+    public Singleton(String value) {
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        Singleton result  = instance;
+        if(result != null){
+            return result;
+        }
+        synchronized (Singleton.class){
+            if(instance == null){
+                instance = new Singleton(value);
+            }
+            return instance;
+        }
+    }
+}
